14604ed9e996a2ab9afc1685ce324452a902ea9c
SANYAM AGARWAL <sanyam.20209@gmail.com>
2016-09-21 22:46:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,16 +5,72 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+     title : 'Article One | Sanyam Agarwal',
+     heading : 'Article One',
+     date:'September 21 ,2016',
+     content : '  Welcome to article one . '
+    
+};
+
+function createTemplate(data){
+    
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+        var htmlTemplate = 
+            `
+            <html>
+            <head>
+             <title>
+                 ${title}
+             </title>  
+             
+             <link href ="/ui/style.css" rel="stylesheet" />
+            </head>
+            
+            <body>
+                <div class = "container">
+                    <div>
+                        <a href='/'> HOME </a>
+                    </div> 
+                    
+                    <hr/>
+                    
+                    <h3>
+                        ${heading}
+                    </h3>
+                    <div>
+                       ${date}
+                        
+                    </div>
+                    <div>
+                        <p>
+                          ${content}
+                        </p>    
+                    </div>
+                </div>    
+                
+            </body>    
+        
+           </html>
+         `;
+    return htmlTemplate;    
+    
+}
 app.get('/article-one', function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(article-one.html));
 });
 
 app.get('/article-two', function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+  res.send(createTemplate(article-two.html));
 });
 
 app.get('/article-three', function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'atricle-three.html'));
+   res.send(createTemplate(article-three.html));
 });
 
 app.get('/', function (req, res) {
